035ff330ccaadc63fee324a20855017e0bcee173
abbas <abbashamidicr@gmail.com>
2024-07-31 17:54:47 +0430

    first commit

---
+++ add.js
@@ -0,0 +1,29 @@
+// Initialize CKEditor
+let editorInstance;
+ClassicEditor
+    .create(document.querySelector("#editor"), {
+        toolbar: [] // Hides the toolbar
+    })
+    .then(editor => {
+        editorInstance = editor; // Assign the editor instance to the variable
+        console.log("Editor was initialized", editor);
+    })
+    .catch(error => {
+        console.error("Error initializing CKEditor:", error);
+    });
+
+// Handle button click event
+const storeButton = document.getElementById("storeButton");
+const editor = document.getElementById('editor');
+storeButton.addEventListener('click', () => {
+    if (editorInstance) {
+        const content = editorInstance.getData(); // Correct method name
+        localStorage.setItem('note', content);
+        editorInstance.setData('');
+        alert('your note got save successfuly');
+        history.go(-1);
+    } else {
+        alert('Editor is not initialized yet.');
+    }
+
+});
